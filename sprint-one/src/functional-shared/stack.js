de5d99f002d88d2f1208ffb5e66ca10703cc2247
Sean Kim <skim2636@gmail.com>
2015-02-07 15:16:36 -0800

    funcShared fixed

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,41 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stack = {};
+  
+  stack.storage = {};
+  stack.end = 0;
+
+  extend(stack, stackMethods);
+
+  return stack;
+};
+
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
 };
 
 var stackMethods = {};
+
+stackMethods.size = function() {
+  return this.end > 0 ? this.end : 0;
+}
+
+stackMethods.push = function(value) {
+  this.storage[this.end++] = value;
+}
+
+stackMethods.pop = function() {
+  if (this.end > 0) {
+    this.end--;    
+  }
+  
+  var popped = this.storage[this.end];
+  delete this.storage[this.end];
+
+  return popped;
+}
 
 
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,23 @@ var Stack = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var end = 0;
 
   // Implement the methods below
   someInstance.push = function(value){
+    end++;
+    storage[end] = value;
   };
 
   someInstance.pop = function(){
+    var popped = storage[end];
+    delete popped;
+    end--;
+    return popped;
   };
 
   someInstance.size = function(){
+    return end < 1 ? 0 : end;
   };
 
   return someInstance;
