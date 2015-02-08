27c6636c883c61025612ea1de98825fb386118a5
Sean Kim <skim2636@gmail.com>
2015-02-07 16:46:32 -0800

    Functional-shared Queue and Stack fixed

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,35 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stack = Object.create(stackMethods);
+
+  stack.storage = {};
+  stack.end = 0;
+
+  return stack;
 };
 
 var stackMethods = {};
+
+stackMethods.push = function(value) {
+  this.storage[this.end++] = value;
+};
+
+stackMethods.pop = function() {
+  // if the stack is not empty
+  if (this.end !== 0) {
+    // delete the element on the end
+    // decrement the index
+    var popped = this.storage[--this.end];
+    delete this.storage[this.end];
+
+    // return the popped element
+    return popped;
+  }
+};
+
+stackMethods.size = function() {
+  return this.end;
+};
 
 
