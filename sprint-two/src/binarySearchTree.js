9b07012df7ca4516f2b520b66b83fcd0266c91a2
Sean Kim <skim2636@gmail.com>
2015-02-09 01:08:25 -0800

    fixed binary search tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,59 @@
 var BinarySearchTree = function(value){
+  var tree = Object.create(BinarySearchTree.prototype);
+  tree.value = value;
+  tree.left = null;
+  tree.right = null;
+  return tree;
+};
+
+/*
+ * Complexity: What is the time complexity of the above functions?
+ */
+BinarySearchTree.prototype.insert = function(value){
+  var newNode = BinarySearchTree(value);
+  var helper = function(treeNode){
+    if(value > treeNode.value){
+      if(treeNode.right === null){
+        treeNode.right = newNode;
+      } else {
+        helper(treeNode.right);
+      }
+    } else {
+      if(treeNode.left === null){
+        treeNode.left = newNode;
+      } else {
+        helper(treeNode.left);
+      }
+    }
+  };
+  helper(this);
+};
+
+BinarySearchTree.prototype.contains = function(target){
+  var check = this;
+  while(check !== null){
+    if(check.value === target){
+      return true;
+    } else if(check.value > target){
+      check = check.left;
+    } else {
+      check = check.right;
+    }
+  }
+  return false;
+};
+
+BinarySearchTree.prototype.depthFirstLog = function(callBack){
+  var recursion = function(treeNode){
+    callBack(treeNode.value);
+    if(treeNode.left !== null){
+      recursion(treeNode.left);
+    }
+    if(treeNode.right !== null){
+      recursion(treeNode.right);
+    }
+  };
+  recursion(this);
 };
 
 
