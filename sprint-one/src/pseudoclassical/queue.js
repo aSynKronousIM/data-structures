502f3e63eb357dd44948b44337720d52e0998dd1
Sean Kim <skim2636@gmail.com>
2015-02-08 16:51:24 -0800

    fixed Pseudoclassical queue and stack

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,25 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.start = 0;
+  this.end = 0;
 };
 
+Queue.prototype.size = function() {
+  return this.end - this.start > 0 ? this.end - this.start : 0;
+};
+
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.end++] = value;
+};
+
+Queue.prototype.dequeue = function() {
+  if (this.end > 0) {
+    var dequeued = this.storage[this.start];
+    delete this.storage[this.start++];
+
+    return dequeued;
+  }
+}
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,24 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.end = 0;
 };
 
+Stack.prototype.size = function() {
+  return this.end;
+};
+
+Stack.prototype.push = function(value) {
+  this.storage[this.end++] = value;
+}
+
+Stack.prototype.pop = function() {
+  if (this.end > 0) {
+    var popped = this.storage[--this.end];
+    delete this.storage[this.end];
+
+    return popped;
+  }
+}
 
