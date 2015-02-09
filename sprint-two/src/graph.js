988a8384b9a5f803e3f775382d71603792fe1b0b
Sean Kim <skim2636@gmail.com>
2015-02-08 23:31:54 -0800

    fixed tree and graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,27 +1,38 @@
 
 
 var Graph = function(){
+  this.storage = {};
 };
 
 Graph.prototype.addNode = function(node){
+  this.storage[node] = node;
 };
 
 Graph.prototype.contains = function(node){
+  return !!this.storage[node];
 };
 
 Graph.prototype.removeNode = function(node){
+  delete this.storage[node];
 };
 
 Graph.prototype.hasEdge = function(fromNode, toNode){
+  return this.storage[fromNode] === this.storage[toNode];
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
+  this.storage[fromNode] = this.storage[toNode];
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
+  this.storage[fromNode] = fromNode;
+  this.storage[toNode] = toNode;
 };
 
 Graph.prototype.forEachNode = function(cb){
+  for (var key in this.storage) {
+    cb(this.storage[key], key, this.storage);
+  }
 };
 
 /*
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,24 +3,66 @@ var Tree = function(value){
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
+  extend(newTree, treeMethods);
 
   return newTree;
 };
 
-
-
-
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
+}
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  var child = Tree(value);
+  this.children.push(child);
 };
 
 treeMethods.contains = function(target){
+  var willVisit = new Queue();
+  willVisit.enqueue(this.children);
 
+  while (willVisit.size() !== 0) {
+    var visiting = willVisit.dequeue();
+
+    for (var i = 0; i < visiting.length; i++) {
+      if (visiting[i].value === target) {
+        return true;
+      }
+      willVisit.enqueue(visiting[i].children);
+    }
+  }
+  return false;
 };
+
+var Queue = function() {
+  this.storage = {};
+  this.start = 0;
+  this.end = 0;
+};
+
+Queue.prototype.size = function(){
+  return this.end - this.start;
+};
+
+Queue.prototype.enqueue = function(value){
+  this.storage[this.end] = value;
+  this.end++;
+};
+
+Queue.prototype.dequeue = function(){
+  var result = this.storage[this.start];
+  if(this.start !== this.end){
+    delete this.storage[this.start];
+    this.start++;
+  }
+  return result;
+};
+
 
 
 /*
