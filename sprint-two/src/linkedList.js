3d2a4165edc9787f21e9430c388cbca82c0c7421
Sean Kim <skim2636@gmail.com>
2015-02-08 18:01:35 -0800

    fixed linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,38 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var newNode = Node(value);
+
+    if (list.head === null && list.tail === null) {
+      list.head = newNode;
+      list.head.next = newNode;
+      list.tail = newNode;
+    }
+
+    list.tail.next = newNode;
+    list.tail = newNode;
   };
 
   list.removeHead = function(){
+    var oldHead = list.head;
+    list.head = list.head.next;
+    oldHead.next = null;
+
+    return oldHead.value;
   };
 
   list.contains = function(target){
+    var check = list.head;
+
+    while (check !== null) {
+      if (check.value === target) {
+        return true;
+      } else {
+        check = check.next;
+      }
+    }
+
+    return false;
   };
 
   return list;
